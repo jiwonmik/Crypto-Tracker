71b1db3abb1ffe40e0380b3a53beff208c6fb601
jiwonmik <jiwonmik@gmail.com>
2023-01-15 22:01:36 +0900

    Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import styled, { keyframes } from "styled-components";
-import Circle from "./Circle";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange=(event:React.FormEvent<HTMLInputElement>)=>{
+    const {
+      currentTarget: {value},
+    } = event;
+    setValue(value);
+  };
+  const onSubmit=(event:React.FormEvent<HTMLFormElement>)=>{
+    event.preventDefault();
+    console.log("Hello", value);
+  };
   return (
     <div>
-      <Circle bgColor="teal" borderColor="yellow"/>
-      <Circle bgColor="tomato" text="I'm here"/>
+      <form onSubmit={onSubmit}>
+        <input 
+          value={value} 
+          onChange={onChange} 
+          type="text" 
+          placeholder="username"/>
+        <button>Log In</button>
+      </form>
     </div>
   );
 }
