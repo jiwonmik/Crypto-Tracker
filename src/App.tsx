91dd9acd8c5520c3a2695606f4e2ad76466bb7db
jiwonmik <jiwonmik@gmail.com>
2023-01-15 23:06:38 +0900

    Crypto Tracker setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
 import styled, { keyframes } from "styled-components";
 import { useState } from "react";
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-interface DummyProps {
-  text: string;
-  active ?: boolean;
-}
-
-function Dummy({text = "default", active=false}:DummyProps){
-  return <h1>{text}</h1>
-}
+import Router from "./Router";
 
 function App() {
   return (
-    <Container>
-      <Dummy active text="hello"/>
-    </Container>
+    <Router/>
   );
 }
 
