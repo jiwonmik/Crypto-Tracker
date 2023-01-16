96cacd325c8ed5f6f0b10e4eac11d92fcabd00b3
jiwonmik <jiwonmik@gmail.com>
2023-01-16 18:37:08 +0900

    react query for coin screen

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -64,17 +64,7 @@ interface ICoin {
 }
 function Coins() {
     const { isLoading, data} = useQuery<ICoin[]>("allCoins", fetchCoins) 
-    /* const [coins, setCoins]=useState<ICoin[]>([]);
-    const [loading, setLoading] =useState(true);
-    useEffect(() =>{
-        (async()=>{
-            const response = await fetch("https://api.coinpaprika.com/v1/coins")
-            const json = await response.json();
-            console.log(json);
-            setCoins(json.slice(0,100));
-            setLoading(false);
-        })();
-    },[]) */
+
     return (
     <Container>
         <Header>
