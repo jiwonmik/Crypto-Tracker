65e5fa24a96a3c2298e8a54aa9c08b55f64a5b36
jiwonmik <jiwonmik@gmail.com>
2023-01-16 17:39:55 +0900

    Nested Routes for chart and price tab

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,16 +1,15 @@
 import { Switch, Route, useLocation, useParams } from "react-router";
 import styled from "styled-components";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
+import { Link, useRouteMatch } from "react-router-dom";
 import Chart from "./Chart";
 import Price from "./Price";
-
 
 const Container = styled.div`
     padding: 0px 20px;
     max-width: 480px;
     margin: 0 auto;
 `;
-
 const Header = styled.header`
     height: 15vh;
     display: flex;
@@ -19,14 +18,12 @@ const Header = styled.header`
 `;
 const Title = styled.h1`
     font-size: 48px;
-    color: ${(props)=>props.theme.accentColor}
+    color: ${(props) => props.theme.accentColor}
 `;
-
 const Loader = styled.span`
     text-align: center;
     display: block;    
 `;
-
 const Overview = styled.div`
     display: flex;
     justify-content: space-between;
@@ -34,7 +31,6 @@ const Overview = styled.div`
     padding: 10px 20px;
     border-radius: 10px;
 `;
-
 const OverviewItem = styled.div`
   display: flex;
   flex-direction: column;
@@ -44,11 +40,31 @@ const OverviewItem = styled.div`
     font-size: 10px;
     font-weight: 400;
     text-transform: uppercase;
-    margin-bottm: 5px;
+    margin-bottom: 5px;
   }
 `;
 const Description = styled.p`
     margin: 20px 0px;
+`
+const Tabs = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    margin: 25px 0px;
+    gap: 10px;
+`;
+
+const Tab = styled.span<{ isActive: boolean }>`
+    text-align: center;
+    text-transform: uppercase;
+    font-size: 12px;
+    font-weight: 400;
+    background-color: rgba(0,0,0,0.5);
+    padding: 7px 0px;
+    border-radius: 10px;
+    color: ${(props) => props.isActive ? props.theme.accentColor : props.theme.textColor};
+    a {
+        display: block;
+    }
 `
 
 interface RouteParams {
@@ -82,7 +98,7 @@ interface IInfoData {
     last_data_at: string;
 }
 
-interface IPriceData{
+interface IPriceData {
     id: string;
     name: string;
     symbol: string;
@@ -94,7 +110,7 @@ interface IPriceData{
     first_data_at: string;
     last_updated: string;
     quotes: {
-        USD:{
+        USD: {
             ath_date: string;
             ath_price: number;
             market_cap: number;
@@ -122,11 +138,14 @@ function Coin() {
     const { state } = useLocation<RouteState>();
     const [info, setInfo] = useState<IInfoData>();
     const [priceInfo, setPriceInfo] = useState<IPriceData>();
-    useEffect(()=>{
-        (async()=>{
-            const infoData = await(
+    const priceMatch = useRouteMatch(`/${coinId}/price`);
+    const chartMatch = useRouteMatch(`/${coinId}/chart`);
+
+    useEffect(() => {
+        (async () => {
+            const infoData = await (
                 (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`))
-                ).json()
+            ).json()
             console.log(infoData);
             const priceData = await (
                 (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`))
@@ -135,51 +154,60 @@ function Coin() {
             setPriceInfo(priceData);
             setLoading(false);
         })();
-    },[coinId]);
+    }, [coinId]);
+
     return (
         <Container>
             <Header>
                 <Title>
-                {state?.name ? state.name : loading ? "Loading..." : info?.name}
+                    {state?.name ? state.name : loading ? "Loading..." : info?.name}
                 </Title>
             </Header>
             {loading ? (
                 <Loader>Loading...</Loader>
             ) : (
                 <>
-                <Overview>
-                    <OverviewItem>
-                        <span>Rank:</span>    
-                        <span>{info?.rank}</span>
-                    </OverviewItem>
-                    <OverviewItem>
-                        <span>Symbol:</span>
-                        <span>{info?.symbol}</span>
-                    </OverviewItem>
-                    <OverviewItem>
-                        <span>Open Source:</span>
-                        <span>{info?.open_source ? "YES" : "NO"}</span>
-                    </OverviewItem>
-                </Overview>
-                <Description>{info?.description}</Description>
-                <Overview>
-                    <OverviewItem>
-                    <span>Total Supply:</span>
-                    <span>{priceInfo?.total_supply}</span>
-                    </OverviewItem>
-                    <OverviewItem>
-                    <span>Max Supply:</span>
-                    <span>{priceInfo?.max_supply}</span>
-                    </OverviewItem>
-                </Overview>
-                <Switch>
-                    <Route path={`/${coinId}/price`}>
-                        <Price />
-                    </Route>
-                    <Route path={`/${coinId}/chart`}>
-                        <Chart />
-                    </Route>
-                </Switch>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Rank:</span>
+                            <span>{info?.rank}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Symbol:</span>
+                            <span>{info?.symbol}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Open Source:</span>
+                            <span>{info?.open_source ? "YES" : "NO"}</span>
+                        </OverviewItem>
+                    </Overview>
+                    <Description>{info?.description}</Description>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Total Supply:</span>
+                            <span>{priceInfo?.total_supply}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Max Supply:</span>
+                            <span>{priceInfo?.max_supply}</span>
+                        </OverviewItem>
+                    </Overview>
+                    <Tabs>
+                        <Tab isActive={chartMatch !== null}>
+                            <Link to={`/${coinId}/chart`}>Chart</Link>
+                        </Tab>
+                        <Tab isActive={priceMatch !== null}>
+                            <Link to={`/${coinId}/price`}>Price</Link>
+                        </Tab>
+                    </Tabs>
+                    <Switch>
+                        <Route path={`/:coinId/price`}>
+                            <Price />
+                        </Route>
+                        <Route path={`/:coinId/chart`}>
+                            <Chart />
+                        </Route>
+                    </Switch>
                 </>
             )}
         </Container>
--- src/theme.tsx
+++ src/theme.tsx
@@ -2,6 +2,6 @@ import { DefaultTheme } from "styled-components";
 
 export const lightTheme: DefaultTheme = {
     bgColor: "#505252",
-    textColor: "#fef073",
+    textColor: "#ffffff",
     accentColor: "#9e52fb",
 }
